09b7a6d62ab33fb18fde4164eb737c1508e08e5c
Elena Nurullina <elenka_san@yahoo.com>
2021-10-12 12:17:26 -0400

    app

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import requests
 # from secrets import API_KEY
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres:///recipeTips')
-# app.config['SQLALCHEMY_DATABASE_URI'] = (
-#    os.environ.get('DATABASE_URL', 'postgresql:///recipeTips'))
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgres:///recipeTips')
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+   os.environ.get('DATABASE_URL', 'postgresql:///recipeTips'))
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'is a secret')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
