59dbfe2de1a33e027d0dab445244e77f6b9e195e
Elenka^_^San <75818489+ElenkaSan@users.noreply.github.com>
2022-06-28 11:04:20 -0400

    Delete secrets.py

--- secrets.py
+++
@@ -1,8 +0,0 @@
-import os
-
-API_KEY = '00f4865fadd742e1b6619374dbf539cc'   #yahoo
-# API_KEY = '812caf8c30e74890b0eded03076a94de'  #mail
-# API_KEY = ' 5d52626b0a8b4f4b9d5c9b0f749fc5bb '   #gmail
-
-BASE_URL = os.environ.get('BASE_URL')
-API_KEY = os.environ.get('KEY')
